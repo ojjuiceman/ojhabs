4d811b96ffd0b08c29b5dee03cb57c0ce87c65b0
Okera Johnson <okerajohnson@gmail.com>
2023-03-23 19:34:54 -0400

    addeddd ssome stars

--- habsdraft/src/rehabs.jsx
+++ habsdraft/src/rehabs.jsx
@@ -1,16 +1,20 @@
-import React from 'react'
+import * as React from 'react'
+import StarRating from "./StarRating.jsx"
+
+
 
 
 const UserProfile = (props) => {
   return (
     <div>
-        <h1>Personal Information</h1>
+        <h1>New York Rehabs</h1>
         <ul className="profile-list">
           {
             props.data.map (content =>(
               <li>
-                <span><strong>ID:</strong> {content.PROVIDER_NAME}</span>
-               \
+                <span><strong>ID:</strong> {content._id.$oid}</span>
+                <StarRating />
+              
               </li>
             ))
           }
