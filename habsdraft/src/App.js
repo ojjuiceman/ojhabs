24ea428ba97977f1deec6ddd57b4253e55db6593
Okera Johnson <okerajohnson@gmail.com>
2023-03-25 09:52:55 -0400

    okta authorization implementation

--- habsdraft/src/App.js
+++ habsdraft/src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import data from './api/nyrehabs.json'
 import UserProfile from './rehabs'
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+import { SecureRoute, LoginCallback } from '@okta/okta-react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/rating">Rate</Link>
+      </nav>
+      <main>
+        <Route exact path="/" component={()=> 'Home Page'} />
+        <SecureRoute exact path="/rating" component={()=>'Rating Page'} />
+        <Route path="/login/callback" component={LoginCallback} />
       
       <UserProfile data={data} />
+      </main>
     </div>
   );
 }
--- habsdraft/src/index.js
+++ habsdraft/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import { Security } from '@okta/okta-react';
+
+const oktaConfig = {
+  issuer: `${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`,
+  redirect_uri: `${window.location.origin}/implicit/callback`,
+  client_id: process.env.REACT_APP_OKTA_CLIENT_ID,
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
+  <Security {...oktaConfig}>
     <App />
-  </React.StrictMode>
+  </Security>
+</BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
